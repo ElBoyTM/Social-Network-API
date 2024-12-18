dc35b8ba784ab1faca49b5efdcbd6e05f6292f4f
ElBoyTM <nszalenski@gmail.com>
2024-12-18 13:44:59 -0500

    filled out Thought.ts

--- App/src/models/Thought.ts
+++ App/src/models/Thought.ts
@@ -1,1 +1,74 @@
 import { Schema, Types, model, Document } from 'mongoose';
+
+interface IThought extends Document {
+    thoughtText: string,
+    createdAt: Date,
+    username: string,
+    reactions: IReaction[]
+}
+
+interface IReaction extends Document {
+    reactionId: Schema.Types.ObjectId,
+    reactionBody: string,
+    username: string,
+    createdAt: Date
+}
+
+const reactionSchema = new Schema<IReaction>(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    {
+        timestamps: true,
+        _id: false
+    }
+);
+
+const thoughtSchema = new Schema<IThought>({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model<IThought>('Thought', thoughtSchema);
+
+export default Thought;
