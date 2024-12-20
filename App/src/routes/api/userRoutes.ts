a3613aaaedcdc27518767241644ea21942da3375
ElBoyTM <nszalenski@gmail.com>
2024-12-20 16:20:20 -0500

    filled out user routes

--- App/src/routes/api/userRoutes.ts
+++ App/src/routes/api/userRoutes.ts
@@ -4,7 +4,31 @@ import {
   getAllUsers,
   getUserById,
   createUser,
+    updateUser,
   deleteUser,
   addFriend,
   deleteFriend,
 } from "../../controllers/userController.js";
+
+// /api/users
+router.route("/").get(getAllUsers);
+
+// /api/users/:userId
+router.route("/:userId").get(getUserById);
+
+// POST a new user
+router.route("/").post(createUser);
+
+// PUT to update a user by their ID
+router.route("/:userId").put(updateUser);
+
+// DELETE a user by their ID
+router.route("/:userId").delete(deleteUser);
+
+// /api/users/:userId/friends
+router.route("/:userId/friends").post(addFriend);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").delete(deleteFriend);
+
+export { router as userRouter };
