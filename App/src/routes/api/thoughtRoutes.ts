db3aea887eb49c6b0558da6b2f264c1fbff2bb7f
ElBoyTM <nszalenski@gmail.com>
2024-12-20 16:28:52 -0500

    filled out thought routes

--- App/src/routes/api/thoughtRoutes.ts
+++ App/src/routes/api/thoughtRoutes.ts
@@ -9,3 +9,26 @@ import {
   createReaction,
   deleteReaction
 } from '../../controllers/thoughtController';
+
+// GET all thoughts /thoughts
+router.get('/', getThoughts);
+
+// GET thought by id /thoughts/:thoughtId
+router.get('/:thoughtId', getThoughtById);
+
+// POST create a thought /thoughts
+router.post('/', createThought);
+
+// PUT update a thought /thoughts/:thoughtId
+router.put('/:thoughtId', updateThought);
+
+// DELETE a thought /thoughts/:thoughtId
+router.delete('/:thoughtId', deleteThought);
+
+// POST create a reaction /thoughts/:thoughtId/reactions
+router.post('/:thoughtId/reactions', createReaction);
+
+// DELETE a reaction /thoughts/:thoughtId/reactions/:reactionId
+router.delete('/:thoughtId/reactions/:reactionId', deleteReaction);
+
+export { router as thoughtRouter };
